d95d2381019fff2469197b1b98776892120c8857
Dev-code457 <dev.goodpegg@gmail.com>
2024-11-11 16:10:18 +0530

    Initial

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,19 @@ export function middleware(request: NextRequest) {
   console.log("Middleware executed");
 
   const token = request.cookies.get("__session")?.value;
+  console.log(`Token: ${token}, Pathname: ${request.nextUrl.pathname}`);
 
   const pathname = request.nextUrl.pathname;
-
   const isLoginPage = pathname === "/";
 
   if (!token) {
     if (!isLoginPage) {
+      console.log("Redirecting to login page");
       return NextResponse.redirect(new URL("/", request.url));
     }
   } else {
     if (isLoginPage) {
+      console.log("Redirecting to AddNumber page");
       return NextResponse.redirect(new URL("/AddNumber", request.url));
     }
   }
@@ -25,6 +27,7 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    "/",
     "/CheckInOut/:path*",
     "/AddNumber/:path*",
     "/Promotion-Offer/:path*",
